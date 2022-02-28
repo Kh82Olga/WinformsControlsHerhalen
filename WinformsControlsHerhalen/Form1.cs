866c47014b2f0aa9284770ec9f49d1005e9bcfce
kh82olga@protonmail.com <kh82olga@protonmail.com>
2022-02-28 11:38:27 +0100

    bij iedere methode een nieuwe instantie maken

--- WinformsControlsHerhalen/Form1.cs
+++ WinformsControlsHerhalen/Form1.cs
@@ -16,7 +16,7 @@ namespace WinformsControlsHerhalen
         {
             InitializeComponent();
         }
-        
+        //bij iedere methode een nieuwe instantie maken
         private void Form1_Load(object sender, EventArgs e)
         {
             SeedCountry seedCountry = new SeedCountry();
